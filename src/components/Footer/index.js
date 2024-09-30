8f89be8fd9fa693a9378efe8bccb213de9e524d2
Gilvan-R-A <gilvan.dev@gmail.com>
2024-09-30 14:49:39 -0300

    feat: creating the footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Top Music &copy; Desenvolvido por Gilvan Alves 2024</h2>
+        </footer>
+    );
+}
+
+export default Footer;
