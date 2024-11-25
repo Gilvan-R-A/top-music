1045fb9eb8f644cbcc471a889f81e78182db68a1
Gilvan-R-A <gilvan.dev@gmail.com>
2024-11-25 09:07:43 -0300

    feat: Banner Component creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Banner from "./components/Banner";
 
 function App() {
   return (
           <>
           <Header />
+          <Banner image="home" />
           <h1>Hello World!</h1>
           <p>Olá Mundo! Estou aprendendo React JS</p>
           <Footer />
