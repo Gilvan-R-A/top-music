a97a6d7a5d27b1139adc0a1988ca8fc15c4ec0f3
Gilvan-R-A <gilvan.dev@gmail.com>
2024-11-25 09:25:21 -0300

    feat: Container Component creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
           <>
           <Header />
           <Banner image="home" />
-          <h1>Hello World!</h1>
-          <p>Olá Mundo! Estou aprendendo React JS</p>
+          <Container>
+            <h1>Hello World!</h1>
+            <p>Olá Mundo! Estou aprendendo React JS</p>
+          </Container>
           <Footer />
           </>
           
